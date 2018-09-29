2e88c1fee6e37ae44a046feb5dba94af39cb7c78
xTEddie <edwardtranv1@gmail.com>
2018-09-29 14:55:18 -0400

    Update video frame model to store current second using float field instead

--- server/apps/videos/models/videoframe.py
+++ server/apps/videos/models/videoframe.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class VideoFrame(models.Model):
     video = models.ForeignKey('videos.video', related_name='videoframes', on_delete=models.CASCADE, blank=True)
-    current_time = models.CharField(max_length=255)
+    current_second = models.FloatField()
     violence_status = models.BooleanField(default=True)
 
     def __str__(self):
