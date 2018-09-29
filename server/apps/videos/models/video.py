e2a09222bdfa909840c1e22e3440d573bc54a39c
xTEddie <edwardtranv1@gmail.com>
2018-09-29 12:59:56 -0400

    Rename function

--- server/apps/videos/models/video.py
+++ server/apps/videos/models/video.py
@@ -1,8 +1,8 @@
 import os
 from django.db import models
 
-def get_image_path(instance, filename):
+def get_video_path(instance, filename):
     return os.path.join('videos', filename)
 
 class Video(models.Model):
-    path = models.FileField(upload_to=get_image_path, blank=False, null=False)
+    path = models.FileField(upload_to=get_video_path, blank=False, null=False)
