6a5463ebfbf18fe6e11c0364738a2ee888343fa4
Meko Deng <mekodeng94@gmail.com>
2018-09-29 23:25:08 -0400

    Watashiwa Meko Baka

--- server/scripts/vid_to_word.py
+++ server/scripts/vid_to_word.py
@@ -1,43 +1,71 @@
 from google.cloud import videointelligence
 import os 
+import json
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=r"C:\Users\mingh\OneDrive\Documents\code\implement_ai\Implementai2018-7e133461c5fc.json"
-vid_path = r"C:\Users\mingh\OneDrive\Documents\code\implement_ai\server\media\videos"
+vid_path = r"C:\Users\mingh\OneDrive\Documents\code\implement_ai\server\utils\videos"
 
 video_client = videointelligence.VideoIntelligenceServiceClient()
 features = [videointelligence.enums.Feature.LABEL_DETECTION]
 
-for vid in os.listdir(vid_path):
-    os.chdir(vid_path)
-    with open(vid, 'rb') as movie:
-        input_content = movie.read()
+final_list = []
 
-    operation = video_client.annotate_video(
-    features=features, input_content=input_content)
+def extract_information():
+    for vid in os.listdir(vid_path):
+        os.chdir(vid_path)
+        attributes = []
+        with open(vid, 'rb') as movie:
+            input_content = movie.read()
+            file_name = os.path.splitext(vid)[0]         #file_name
 
-    # operation = video_client.annotate_video(
-    #     'gs://demomaker/cat.mp4', features=features)
-    print('\nProcessing video for label annotations:')
+        attributes.append(file_name)
 
-    result = operation.result(timeout=90)
-    print('\nFinished processing.')
+        operation = video_client.annotate_video(
+        features=features, input_content=input_content)
 
-    # first result is retrieved because a single video was processed
-    segment_labels = result.annotation_results[0].segment_label_annotations
-    for i, segment_label in enumerate(segment_labels):
-        print('Video label description: {}'.format(
-            segment_label.entity.description))
-        for category_entity in segment_label.category_entities:
-            print('\tLabel category description: {}'.format(
-                category_entity.description))
+        print('\nProcessing video {} for label annotations:'.format(file_name))
 
-        for i, segment in enumerate(segment_label.segments):
-            start_time = (segment.segment.start_time_offset.seconds +
-                        segment.segment.start_time_offset.nanos / 1e9)
-            end_time = (segment.segment.end_time_offset.seconds +
-                        segment.segment.end_time_offset.nanos / 1e9)
-            positions = '{}s to {}s'.format(start_time, end_time)
-            confidence = segment.confidence
-            print('\tSegment {}: {}'.format(i, positions))
-            print('\tConfidence: {}'.format(confidence))
-        print('\n')
+        result = operation.result(timeout=1000)
+        print('\nFinished processing.')
+
+        segment_labels = result.annotation_results[0].segment_label_annotations
+
+        for i, segment_label in enumerate(segment_labels):
+            one_attribute = []
+            label = segment_label.entity.description        #label
+            print('Video label description: {}'.format(
+                segment_label.entity.description))
+            
+            category = ""
+            for category_entity in segment_label.category_entities:
+                category = category_entity.description      #catgory
+                print('\tLabel category description: {}'.format(
+                    category_entity.description))
+
+            confidence = ""
+            for i, segment in enumerate(segment_label.segments):
+                start_time = (segment.segment.start_time_offset.seconds +
+                            segment.segment.start_time_offset.nanos / 1e9)
+                end_time = (segment.segment.end_time_offset.seconds +
+                            segment.segment.end_time_offset.nanos / 1e9)
+                positions = '{}s to {}s'.format(start_time, end_time)
+                confidence = segment.confidence             #confidence
+                print('\tSegment {}: {}'.format(i, positions))
+                print('\tConfidence: {}'.format(confidence))
+            
+            one_attribute.append(label)
+            one_attribute.append(category)
+            one_attribute.append(confidence)
+
+            attributes.append(one_attribute)
+            print('\n')
+        
+        final_list.append(attributes)            
+    
+    print(final_list)
+    with open('data.json', 'w') as outfile:
+        json.dump(final_list, outfile)
+    print('done')    
+
+if __name__ == "__main__":
+    extract_information()
