c804c39cf7032dc09e180562d81fe0bc4420dd8a
Meko Deng <mekodeng94@gmail.com>
2018-09-30 09:20:35 -0400

    backend updated with skeleton

--- frontend/src/components/Frontend.vue
+++ frontend/src/components/Frontend.vue
@@ -25,6 +25,15 @@ export default {
         '2': false,
         '3': false,
         '4': false,
+        '5': false,
+        '6': false,
+        '7': false,
+        '8': false,
+        '9': false,
+        '10': false,
+        '11': false,
+        '12': false,
+        '13': false
       },
     }
   },
